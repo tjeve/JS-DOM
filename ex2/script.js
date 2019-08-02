a962a4d6796d5a9651897e9af775b9a17337a7d2
Terrence Eve <tijayeve@gmail.com>
2019-08-01 23:13:32 -0500

    Added coments to javascript explaining what you were doing

--- ex2/script.js
+++ ex2/script.js
@@ -1,5 +1,5 @@
 // Change the background color of the box to blue
 
-var box = document.getElementById("box")
+var box = document.getElementById("box")    //simplifies the name of the box
 
-box.style.backgroundColor = "blue"
+box.style.backgroundColor = "blue" //changes the backgroundColor property to blue
--- ex3/script.js
+++ ex3/script.js
@@ -3,5 +3,5 @@
 var button = document.getElementById("myButton");  //named the button
 
 button.addEventListener('click', function(){        //listens for the button being clicked, turns it blue, 
-    box.style.backgroundColor = "blue"
+    box.style.backgroundColor = "blue"              //Changes the backgroundColor property of the element to Blue
 })
