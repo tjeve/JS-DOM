62eaaddbb37a6df9a787896bfa18f37344a2c02e
Terrence Eve <tijayeve@gmail.com>
2019-08-03 14:31:44 -0500

    completed ex7. You had a spelling error in display

--- ex7/script.js
+++ ex7/script.js
@@ -2,8 +2,27 @@
 // BONUS: Can you make the section slide up and down like this? :
 // https://www.w3schools.com/bootstrap/bootstrap_collapse.asp
 
-var accordian = document.getElementById("accordian")            //sets names for ID's
-var sectionHeader = document.getElementById("section-header")   
-var section = document.getElementById("section")
+var accordion = document.getElementById("accordion")            // sets names for accordion
+var sectionHeader = document.getElementById("section-header")   // sets name for sectionHeader
+var section = document.getElementById("section")                // sets name for section
 
-console.dir(dsection)
+console.log(sectionHeader)
+
+sectionHeader.addEventListener("click", function() {
+    console.dir(accordion)                                          //console dir to search properties of accordion. Do I need this?
+    console.assert(section, "the browser did not find section id")  //an assertion to see if the browser finds the id called section
+
+    // section.classList.toggle("panel")
+
+    // let panel = this.nextElementSibling;
+    if(section.style.display === 'block') {
+        section.style.display = 'none'
+    } else section.style.display = 'block'
+
+
+    // section.style.display = 'none'
+    // sectionHeader.addEventListener("click", function() {
+    //     section.style.display = 'block'
+    // })
+    
+})
