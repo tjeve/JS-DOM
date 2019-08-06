9c1725485aedd4fcf213592bfc5cbeefea9bb7ef
Terrence Eve <tijayeve@gmail.com>
2019-08-06 18:41:23 -0500

    created defintions and addEventListener function for addButton

--- ex8/script.js
+++ ex8/script.js
@@ -5,3 +5,18 @@
     BONUS) Add a functioning "delete" button to each task (HINT: use the keyword "this" in your delete button's click listener! Console log "this" to see what value it holds)
     SUPER BONUS) Make each task editable
 */
+
+// Defines elements
+var addButton = document.getElementById("add-button")
+var newTask = document.getElementById("description")
+var runningList = document.getElementById("todo-list")
+var newListElement = document.createElement("li")
+
+addButton.addEventListener("click", function() {  //Listen for the click of the button and when it happens...
+    console.dir(newListElement)         //console dir the newListElement node
+    newListElement[0].value = newTask.value
+    console.dir(newListElement[0])
+})
+
+// What I want to do here is take the text value of the description and insert it as a new <li></li> element and append it to
+// the todo list.
